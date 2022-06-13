256f286434596c264197039c3697f673cff2c086
Maliyah02 <107362232+Maliyah02@users.noreply.github.com>
2022-06-12 22:15:19 -0400

    Add files via upload
    Part 1

---
+++ main.py
@@ -0,0 +1,11 @@
+user_num = 4
+print('Enter integer:')
+print('You entered:', user_num)
+square_num = user_num * user_num
+print(user_num, 'squared is', square_num)
+cubed_num = user_num * user_num * user_num
+print('And' , user_num , "cubed is",cubed_num, "!!")
+another_num = 5
+print('Enter another integer:')
+print(user_num, '+' , another_num, "is", user_num + another_num)
+print(user_num, "*", another_num, "is" , user_num * another_num)
